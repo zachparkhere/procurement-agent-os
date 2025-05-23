ed8ff3b6e8cda9553614cca66f7f59c3f8accc1a
zachparkhere <zach@shiftsai.com>
2025-05-23 13:38:30 +0900

    path revise

--- mcp_server/main.py
+++ mcp_server/main.py
@@ -1,5 +1,5 @@
 import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
